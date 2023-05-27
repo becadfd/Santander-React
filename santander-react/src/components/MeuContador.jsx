6088181d6cf17e1e7cee557458f5c3f345dfeaba
Rebeca Damas <damas.rebeca@outlook.com>
2023-05-27 19:05:09 -0300

    Renderização Condicional

--- santander-react/src/components/MeuContador.jsx
+++ santander-react/src/components/MeuContador.jsx
@@ -8,10 +8,25 @@ export default function MeuContador() {
     setContador(contador + 1)
   }
 
+  function diminuir() {
+    setContador(contador - 1)
+  }
+
+  if (contador > 9) {
+    return (
+      <div>
+        <h1>Valor muito alto</h1>
+        <button onClick={aumentar}>Aumentar</button>
+        <button onClick={diminuir}>Diminuir</button>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Meu Contador: {contador}</h1>
       <button onClick={aumentar}>Aumentar</button>
+      <button onClick={diminuir}>Diminuir</button>
     </div>
   )
 }
